37640c62e4bb1b2b99512f7d43548a9fe51dbfe8
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-06-06 22:19:25 +0100

    REST Controller Methods to Find and Add Employee
    Demo

--- src/main/java/com/springboot/crudemo/rest/EmployeeRestController.java
+++ src/main/java/com/springboot/crudemo/rest/EmployeeRestController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.springboot.crudemo.dao.EmployeeDAO;
 import com.springboot.crudemo.entity.Employee;
 import com.springboot.crudemo.service.EmployeeService;
 
@@ -28,6 +30,30 @@ public class EmployeeRestController {
 		return employeeService.findAll();
 	}
 	
+	// ADD MAPPING FOR GET "/EMPLOYEES/{EMPLOYEEID}
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		
+		Employee theEmployee = this.employeeService.findById(employeeId);
+		
+		if (theEmployee == null) {
+			throw new RuntimeException("Employee not found - "+employeeId);
+		}
+		
+		return theEmployee;
+		
+	}
+	
+	// ADD MAPPING FOR POST /EMPLOYESS - ADD NEW EMPLOYEE
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+		
+		// ALSO JST IN CASE THEY PASS AND ID JASON - SET TO ID 0
+		theEmployee.setId(0);
+		this.employeeService.addEmployee(theEmployee);
+		
+		return theEmployee;
+	}
 	
 	
 }
